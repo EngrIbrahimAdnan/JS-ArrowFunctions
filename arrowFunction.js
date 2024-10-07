7106af14769078a010166bf1f76cde5462ad4f4b
EngrIbrahimAdnan <ibrahim.a.y.m.98@gmail.com>
2024-10-07 14:22:27 +0300

    Adding file arrow function file

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+const greet= (name)=>{
+    return console.log(`Hello, ${name}!`);
+}
+const Sum=(num1,num2)=>{
+    return num1+num2;
+}
+const square=(num)=>{
+    return Math.pow(num,2);
+}
+const arrSquared = (arr) => arr.map(number => number * number);
+
+//console.log(arrSquared(arr)); - -
